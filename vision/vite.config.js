0067dfe9afe15c99b5a9a4f5d9433120a2b0b2cd
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-18 20:39:34 +0530

    feat: Setup proxy

--- vision/vite.config.js
+++ vision/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import proxyOptions from './proxyOptions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	server: {
+		port: 8080,
+		proxy: proxyOptions
+	},
 	build: {
 		outDir: '../doppio/public/vision',
 		emptyOutDir: true,

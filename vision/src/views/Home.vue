fdb2ad298e9f049cf5e33f25f4e00608b850bba5
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-18 20:37:00 +0530

    feat: Application Shell

--- vision/src/views/Home.vue
+++ vision/src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-	<h1>Home</h1>
+	<div>
+		<header class="bg-white shadow-sm">
+			<div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+				<h1 class="text-lg leading-6 font-semibold text-gray-900">Dashboard</h1>
+			</div>
+		</header>
+		<main>
+			<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+				<div class="px-4 py-4 sm:px-0">
+					<div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+				</div>
+			</div>
+		</main>
+	</div>
 </template>
 
-<script>
-</script>
+<script></script>

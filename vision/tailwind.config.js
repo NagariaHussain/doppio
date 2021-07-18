2af04317dd62d30cc531bf660392630afc8538f6
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-18 14:35:04 +0530

    style: Prettier

--- vision/tailwind.config.js
+++ vision/tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  purge: {
-    content: ['./public/index.html', './src/**/*.html', './src/**/*.vue'],
-    options: {
-      whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/]
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: {
+		content: ['./public/index.html', './src/**/*.html', './src/**/*.vue'],
+		options: {
+			whitelistPatternsChildren: [/chart-container$/, /graph-svg-tip$/],
+		},
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};

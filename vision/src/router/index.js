efd617873e019b6216d5b1934dca58f06950249a
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-28 18:18:20 +0530

    fix: Router history type

--- vision/src/router/index.js
+++ vision/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const routes = [
@@ -11,7 +11,7 @@ const routes = [
 
 const router = createRouter({
 	base: '/vision/',
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 });
 

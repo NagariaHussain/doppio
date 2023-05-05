50c851eb83c83d31b3ccb0c4899fe415db9ae136
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-05-05 19:57:55 +0530

    refactor: reusable utils

--- doppio/commands/spa_generator.py
+++ doppio/commands/spa_generator.py
@@ -1,11 +1,13 @@
-import re
-import json
 import click
 import subprocess
 
 from pathlib import Path
 from .boilerplates import *
-from .utils import create_file
+from .utils import (
+	create_file,
+	add_build_command_to_package_json,
+	add_routing_rule_to_hooks,
+)
 
 
 class SPAGenerator:
@@ -43,14 +45,14 @@ class SPAGenerator:
 			self.create_react_files()
 
 		# Common to all frameworks
-		self.update_package_json()
+		add_build_command_to_package_json(self.app, self.spa_name)
 		self.create_www_directory()
 		self.add_csrf_to_html()
 
 		if self.add_tailwindcss:
 			self.setup_tailwindcss()
 
-		self.add_routing_rule_to_hooks()
+		add_routing_rule_to_hooks(self.app, self.spa_name)
 
 		click.echo(f"Run: cd {self.spa_path.absolute().resolve()} && npm run dev")
 		click.echo("to start the development server and visit: http://<site>:8080")
@@ -136,7 +138,11 @@ class SPAGenerator:
 	def link_controller_files(self):
 		# Link controller files in main.js/main.ts
 		print("Linking controller files...")
-		main_js: Path = self.app_path / (f"{self.spa_name}/src/main.ts" if self.use_typescript else f"{self.spa_name}/src/main.js")
+		main_js: Path = self.app_path / (
+			f"{self.spa_name}/src/main.ts"
+			if self.use_typescript
+			else f"{self.spa_name}/src/main.js"
+		)
 
 		if main_js.exists():
 			with main_js.open("w") as f:
@@ -157,76 +163,15 @@ class SPAGenerator:
 		create_file(proxy_options_file, PROXY_OPTIONS_BOILERPLATE)
 
 	def setup_vue_vite_config(self):
-		vite_config_file: Path = self.spa_path / ("vite.config.ts" if self.use_typescript else "vite.config.js")
+		vite_config_file: Path = self.spa_path / (
+			"vite.config.ts" if self.use_typescript else "vite.config.js"
+		)
 		if not vite_config_file.exists():
 			vite_config_file.touch()
 		with vite_config_file.open("w") as f:
 			boilerplate = VUE_VITE_CONFIG_BOILERPLATE.replace("{{app}}", self.app)
 			boilerplate = boilerplate.replace("{{name}}", self.spa_name)
 			f.write(boilerplate)
-
-	def add_routing_rule_to_hooks(self):
-		hooks_py = Path(f"../apps/{self.app}/{self.app}") / "hooks.py"
-		hooks = ""
-		with hooks_py.open("r") as f:
-			hooks = f.read()
-
-		pattern = re.compile(r"website_route_rules\s?=\s?\[(.+)\]")
-
-		rule = (
-			"{"
-			+ f"'from_route': '/{self.spa_name}/<path:app_path>', 'to_route': '{self.spa_name}'"
-			+ "}"
-		)
-
-		rules = pattern.sub(r"website_route_rules = [{rule}, \1]", hooks)
-
-		# If rule is not already defined
-		if not pattern.search(hooks):
-			rules = hooks + "\nwebsite_route_rules = [{rule},]"
-
-		updates_hooks = rules.replace("{rule}", rule)
-		with hooks_py.open("w") as f:
-			f.write(updates_hooks)
-
-	def update_package_json(self):
-		package_json_path: Path = self.spa_path / "package.json"
-
-		if not package_json_path.exists():
-			print("package.json not found. Please manually update the build command.")
-			return
-
-		data = {}
-		with package_json_path.open("r") as f:
-			data = json.load(f)
-
-		data["scripts"][
-			"build"
-		] = f"vite build --base=/assets/{self.app}/{self.spa_name}/ && yarn copy-html-entry"
-
-		data["scripts"]["copy-html-entry"] = (
-			f"cp ../{self.app}/public/{self.spa_name}/index.html"
-			f" ../{self.app}/www/{self.spa_name}.html"
-		)
-
-		with package_json_path.open("w") as f:
-			json.dump(data, f, indent=2)
-
-		# Update app's package.json
-		app_package_json_path: Path = self.app_path / "package.json"
-
-		if not app_package_json_path.exists():
-			subprocess.run(["npm", "init", "--yes"], cwd=self.app_path)
-
-			data = {}
-			with app_package_json_path.open("r") as f:
-				data = json.load(f)
-
-			data["scripts"]["dev"] = f"cd {self.spa_name} && yarn dev"
-			data["scripts"]["build"] = f"cd {self.spa_name} && yarn build"
-
-			with app_package_json_path.open("w") as f:
-				json.dump(data, f, indent=2)
 
 	def create_www_directory(self):
 		www_dir_path: Path = self.app_path / f"{self.app}/www"
@@ -252,7 +197,8 @@ class SPAGenerator:
 		print("Scaffolding React project...")
 		if self.use_typescript:
 			subprocess.run(
-				["yarn", "create", "vite", self.spa_name, "--template", "react-ts"], cwd=self.app_path
+				["yarn", "create", "vite", self.spa_name, "--template", "react-ts"],
+				cwd=self.app_path,
 			)
 		else:
 			subprocess.run(
@@ -267,7 +213,9 @@ class SPAGenerator:
 		)
 
 	def setup_react_vite_config(self):
-		vite_config_file: Path = self.spa_path / ("vite.config.ts" if self.use_typescript else "vite.config.js")
+		vite_config_file: Path = self.spa_path / (
+			"vite.config.ts" if self.use_typescript else "vite.config.js"
+		)
 		if not vite_config_file.exists():
 			vite_config_file.touch()
 		with vite_config_file.open("w") as f:
--- doppio/commands/utils.py
+++ doppio/commands/utils.py
@@ -1,3 +1,6 @@
+import re
+import json
+import subprocess
 from pathlib import Path
 
 
@@ -10,3 +13,67 @@ def create_file(path: Path, content: str = None):
 	if content:
 		with path.open("w") as f:
 			f.write(content)
+
+
+def add_build_command_to_package_json(app, spa_name):
+	app_path = Path("../apps") / app
+	spa_path: Path = app_path / spa_name
+	package_json_path: Path = spa_path / "package.json"
+
+	if not package_json_path.exists():
+		print("package.json not found. Please manually update the build command.")
+		return
+
+	data = {}
+	with package_json_path.open("r") as f:
+		data = json.load(f)
+
+	data["scripts"][
+		"build"
+	] = f"vite build --base=/assets/{app}/{spa_name}/ && yarn copy-html-entry"
+
+	data["scripts"]["copy-html-entry"] = (
+		f"cp ../{app}/public/{spa_name}/index.html" f" ../{app}/www/{spa_name}.html"
+	)
+
+	with package_json_path.open("w") as f:
+		json.dump(data, f, indent=2)
+
+	# Update app's package.json
+	app_package_json_path: Path = app_path / "package.json"
+
+	if not app_package_json_path.exists():
+		subprocess.run(["npm", "init", "--yes"], cwd=app_path)
+
+		data = {}
+		with app_package_json_path.open("r") as f:
+			data = json.load(f)
+
+		data["scripts"]["dev"] = f"cd {spa_name} && yarn dev"
+		data["scripts"]["build"] = f"cd {spa_name} && yarn build"
+
+		with app_package_json_path.open("w") as f:
+			json.dump(data, f, indent=2)
+
+
+def add_routing_rule_to_hooks(app, spa_name):
+	hooks_py = Path(f"../apps/{app}/{app}") / "hooks.py"
+	hooks = ""
+	with hooks_py.open("r") as f:
+		hooks = f.read()
+
+	pattern = re.compile(r"website_route_rules\s?=\s?\[(.+)\]")
+
+	rule = (
+		"{" + f"'from_route': '/{spa_name}/<path:app_path>', 'to_route': '{spa_name}'" + "}"
+	)
+
+	rules = pattern.sub(r"website_route_rules = [{rule}, \1]", hooks)
+
+	# If rule is not already defined
+	if not pattern.search(hooks):
+		rules = hooks + "\nwebsite_route_rules = [{rule},]"
+
+	updates_hooks = rules.replace("{rule}", rule)
+	with hooks_py.open("w") as f:
+		f.write(updates_hooks)

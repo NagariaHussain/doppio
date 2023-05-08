83f85444c54bb6ce21e63e64963f23bda50512af
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-05-08 12:31:20 +0530

    test: add frappe ui starter

--- doppio/tests/test_spa_generation.py
+++ doppio/tests/test_spa_generation.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from unittest import TestCase
 from doppio.commands.spa_generator import SPAGenerator
+from doppio.commands import add_frappe_ui_starter
 
 
 class TestSPAGeneration(TestCase):
@@ -36,4 +37,24 @@ class TestSPAGeneration(TestCase):
 		self.assertTrue(hooks_py.exists())
 		self.assertTrue("{'from_route': '/dashboard/<path:app_path>', 'to_route': 'dashboard'}" in hooks_py.read_text())
 		
+	def test_add_frappe_ui(self):
+		"""Tests if add_frappe_ui_starter function works as expected"""
+		# run the command
+		add_frappe_ui_starter("frontend", "fake_app")
+
+		# check if the frontend directory was created
+		self.assertTrue(self.app_path.joinpath("frontend").exists())
+
+		# check if the frontend directory contains the correct files
+		self.assertTrue(self.app_path.joinpath("frontend").joinpath("src").exists())
+		self.assertTrue(self.app_path.joinpath("frontend").joinpath("src").joinpath("main.js").exists())
+		self.assertTrue(self.app_path.joinpath("frontend").joinpath("src").joinpath("App.vue").exists())
+
+		# check if package.json has correct build command
+		package_json = self.app_path.joinpath("frontend").joinpath("package.json")
+		self.assertTrue(package_json.exists())
+		self.assertTrue('"build": "vite build --base=/assets/fake_app/frontend/ && yarn copy-html-entry"' in package_json.read_text())
+
+
+
 
